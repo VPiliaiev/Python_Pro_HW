66597979f0dff08efdec2ce7a421ac08856dbbea
Valerii <valerapiliaev@gmail.com>
2024-08-21 13:24:54 +0300

    add dunder method contains

--- HW_5/Python_Pro_Circle.py
+++ HW_5/Python_Pro_Circle.py
@@ -13,13 +13,25 @@ class Circle:
         self.y = y
         self.radius = radius
 
-    def contains(self, point):
+    # def contains(self, point):
+    #     distance = math.sqrt((point.x - self.x) ** 2 + (point.y - self.y) ** 2)
+    #     return distance <= self.radius
+
+    def __contains__(self, point):
         distance = math.sqrt((point.x - self.x) ** 2 + (point.y - self.y) ** 2)
         return distance <= self.radius
 
 
-circle = Circle(0, 0, 3)
+# circle = Circle(0, 0, 3)
+#
+# point = Point(1, 2)
+#
+# print(circle.contains(point))
 
-point = Point(1, 2)
+p1 = Point(1, 2)
+c1 = Circle(1, 2, 10)
+print(p1 in c1)
 
-print(circle.contains(point))
+p2 = Point(12, 2)
+
+print(p2 in c1)
