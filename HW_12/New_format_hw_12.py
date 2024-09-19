985dd15efa53fbab2d909b9c8f021eaf23dae70d
Valerii <valerapiliaev@gmail.com>
2024-09-19 21:47:42 +0300

    Created function new format

---
+++ HW_12/New_format_hw_12.py
@@ -0,0 +1,11 @@
+def new_format(number):
+    return "{:,}".format(int(number)).replace(",", ".")
+
+
+assert (new_format("1000000") == "1.000.000")
+assert (new_format("100") == "100")
+assert (new_format("1000") == "1.000")
+assert (new_format("100000") == "100.000")
+assert (new_format("10000") == "10.000")
+assert (new_format("0") == "0")
+print("finish!")
